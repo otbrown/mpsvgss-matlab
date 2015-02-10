1a1237fb2fcb795f1113bf1959b7eba68f9bba18
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2015-02-10 15:33:31 +0000

    RBlock.m: first working version

--- RBlock.m
+++ RBlock.m
@@ -1,35 +1,15 @@
 % RBlock.m
 % Oliver Thomson Brown
-% 2015-01-15
-%
-% [RETURN]
-% rightBlock	: 3-d double array which contains contraction of sites right of the target site 
-%
-% [ARGUMENTS]
-% mps		: L by 1 cell array containing matrix product state for the system, indexed as mps{site}(:,:,local_state)
-% mpo		: 3 by 1 cell array containing matrix product operator, mpo{1} for first site, mpo{2} for bulk sites, mpo{3} for last site
-% TARGET	: double, integer value -- target site for update during variational search
-%
-% *** IMPORTANT ***
-% For ease of referencing operators are ordered with <0|operator|0> as the top left element!
+% 2015-02-10
 
-function [ rightBlock ] = RBlock(mps, mpo, TARGET)
+function [ rightBlock ] = RBlock( mps, mpo, TARGET )
+	% pull data from inputs
+	L = size( mps, 1 );
+	HILBY = size( mps{L}, 3);
+	OPCOUNT = size( mpo{3}, 1) / HILBY;
 
-	% PULL DATA LIKE TASHA YAR
-	L = size(mps, 1);
-	HILBY = size(mps{L}, 3);
-	OPCOUNT = size(mpo{3}, 1) / HILBY;
-
-	if TARGET == 0
-		inner = ones(1, OPCOUNT, 1);
-	else
-		targRow = size( mps{TARGET}, 1 );
-		targCol = size( mps{TARGET}, 2 );
-		inner = ones(targRow, OPCOUNT, targRow);
-	end
-
-	for site = TARGET + 1 : 1 : L
-		if site == L		% sets matrix product operator choice and sizes
+	for site = L : -1 : TARGET + 1
+		if site == L
 			mpodex = 3;
 			opRowMax = OPCOUNT - 1;
 			opColMax = 0;
@@ -44,67 +24,45 @@ function [ rightBlock ] = RBlock(mps, mpo, TARGET)
 		end
 
 		B = mps{site};
+
+		rowMax = size( B, 1);
+		colMax = size( B, 2);
 		
-		rowMax = size(B, 1);
-		colMax = size(B, 2);
-
-		conjB = zeros(colMax, rowMax, HILBY);
+		conjB = zeros( colMax, rowMax, HILBY);
 		for localState = 1 : 1 : HILBY
-			conjB(:, :, localState) = ctranspose( B(:,:,localState) );
+			conjB( :, :, localState) = ctranspose( B( :, :, localState) );
 		end
-	
-		%rightBlock = sym( zeros(rowMax, OPCOUNT, rowMax) );		% SYM FOR DEBUG PURPOSES ONLY
-		rightBlock = zeros(colMax, OPCOUNT, colMax);  
-%{
-		for row = 1 : 1 : rowMax
-			for opRow = 0 : 1 : opRowMax
-				for conjCol = 1 : 1 : rowMax
-					BWFB = 0;
-					for ketState = 1 : 1 : HILBY
-						for col = 1 : 1 : colMax
-							WFB = 0;
-							for braState = 1 : 1 : HILBY
-								for opCol = 0 : 1 : opColMax
-									FB = 0;
-									for conjRow = 1 : 1 : colMax
-										FB = FB + inner(conjRow, opCol + 1, col) * conjB(conjRow, conjCol, braState);
-									end
-									WFB = WFB + mpo{mpodex}(opRow * HILBY + ketState, opCol * HILBY + braState) * FB;
-								end	% opCol
-							end	% braState
-							BWFB = BWFB + B(row, col, ketState) * WFB;
-						end	% col
-					end	% ketState
-					rightBlock(row, opRow + 1, conjCol) = rightBlock(row, opRow + 1, conjCol) + BWFB;
-				end 	% conjCol
-			end	%opRow
-		end	% row
-%}
 
-		for conjRow = 1 : 1 : colMax				% SUMMATIONS ORDERED AS PRESENTED IN SCHOLLWOECK p65
-			for opCol = 0 : 1 : opColMax
-				for col = 1 : 1 : colMax
+		rightBlock = zeros( rowMax, rowMax, OPCOUNT);
+
+		for conjCol = 1 : 1 : rowMax
+			for opRow = 0 : 1 : opRowMax
+				for row = 1 : 1 : rowMax
 					BWFB = 0;
 					for braState = 1 : 1 : HILBY
-						for conjCol = 1 : 1 : rowMax
+						for conjRow = 1 : 1 : colMax
 							WFB = 0;
 							for ketState = 1 : 1 : HILBY
-								for opRow = 0 : 1 : opRowMax
+								for opCol = 0 : 1 : opColMax
 									FB = 0;
-									for row = 1 : 1 : rowMax
-										FB = FB + inner(conjCol, opRow + 1, row) * B(row, col, ketState);
+									for col = 1 : 1 : colMax
+										if site == L
+											FB = FB + B(row, col, ketState);
+										else
+											FB = FB + inner(conjRow, col, opCol + 1) * B(row, col, ketState);
+										end
 									end
 									WFB = WFB + mpo{mpodex}(opRow * HILBY + braState, opCol * HILBY + ketState) * FB;
 								end
 							end
 							BWFB = BWFB + conjB(conjRow, conjCol, braState) * WFB;
-						end		% conjCol
-					end 		% braState
-					rightBlock(conjRow, opCol + 1, col) = rightBlock(conjRow, opCol + 1, col) + BWFB;
-				end		% col
-			end		%opCol
-		end		% conjRow
+						end
+					end
+					rightBlock(conjCol, row, opRow + 1) = rightBlock(conjCol, row, opRow + 1) + BWFB;
+				end
+			end
+		end
 		inner = rightBlock;
-%		fprintf('site %d contracted\n', site);
-	end	% site  
-end	% function
+		%fprintf('site %d contracted\n', site);
+	end	%site
+end	%function
