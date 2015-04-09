b895afbd0467f2f21bf1ad92fdddc2cc85e3bcb1
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2015-04-09 11:59:55 +0100

    RBlock.m: corrected indentation

--- RBlock.m
+++ RBlock.m
@@ -17,66 +17,65 @@ function [ rightBlock ] = RBlock( mps, mpo, TARGET )
 	if TARGET == L		% protection
 		rightBlock = 1;
 	else
+		HILBY = size( mps{L}, 3);
+		OPCOUNT = size( mpo{3}, 1) / HILBY;
 
-	HILBY = size( mps{L}, 3);
-	OPCOUNT = size( mpo{3}, 1) / HILBY;
+		for site = L : -1 : TARGET + 1
+			if site == L
+				mpodex = 3;
+				opRowMax = OPCOUNT - 1;
+				opColMax = 0;
+			elseif site == 1
+				mpodex = 1;
+				opRowMax = 0;
+				opColMax = OPCOUNT - 1;
+			else
+				mpodex = 2;
+				opRowMax = OPCOUNT - 1;
+				opColMax = OPCOUNT - 1;
+			end
 
-	for site = L : -1 : TARGET + 1
-		if site == L
-			mpodex = 3;
-			opRowMax = OPCOUNT - 1;
-			opColMax = 0;
-		elseif site == 1
-			mpodex = 1;
-			opRowMax = 0;
-			opColMax = OPCOUNT - 1;
-		else
-			mpodex = 2;
-			opRowMax = OPCOUNT - 1;
-			opColMax = OPCOUNT - 1;
-		end
+			B = mps{site};
 
-		B = mps{site};
+			rowMax = size( B, 1);
+			colMax = size( B, 2);
+			
+			conjB = zeros( colMax, rowMax, HILBY);
+			for localState = 1 : 1 : HILBY
+				conjB( :, :, localState) = ctranspose( B( :, :, localState) );
+			end
 
-		rowMax = size( B, 1);
-		colMax = size( B, 2);
-		
-		conjB = zeros( colMax, rowMax, HILBY);
-		for localState = 1 : 1 : HILBY
-			conjB( :, :, localState) = ctranspose( B( :, :, localState) );
-		end
+			rightBlock = zeros( rowMax, rowMax, OPCOUNT);
 
-		rightBlock = zeros( rowMax, rowMax, OPCOUNT);
-
-		for conjCol = 1 : 1 : rowMax
-			for opRow = 0 : 1 : opRowMax
-				for row = 1 : 1 : rowMax
-					BWFB = 0;
-					for braState = 1 : 1 : HILBY
-						for conjRow = 1 : 1 : colMax
-							WFB = 0;
-							for ketState = 1 : 1 : HILBY
-								for opCol = 0 : 1 : opColMax
-									FB = 0;
-									for col = 1 : 1 : colMax
-										if site == L
-											FB = FB + B(row, col, ketState);
-										else
-											FB = FB + inner(conjRow, col, opCol + 1) * B(row, col, ketState);
+			for conjCol = 1 : 1 : rowMax
+				for opRow = 0 : 1 : opRowMax
+					for row = 1 : 1 : rowMax
+						BWFB = 0;
+						for braState = 1 : 1 : HILBY
+							for conjRow = 1 : 1 : colMax
+								WFB = 0;
+								for ketState = 1 : 1 : HILBY
+									for opCol = 0 : 1 : opColMax
+										FB = 0;
+										for col = 1 : 1 : colMax
+											if site == L
+												FB = FB + B(row, col, ketState);
+											else
+												FB = FB + inner(conjRow, col, opCol + 1) * B(row, col, ketState);
+											end
 										end
+										WFB = WFB + mpo{mpodex}(opRow * HILBY + braState, opCol * HILBY + ketState) * FB;
 									end
-									WFB = WFB + mpo{mpodex}(opRow * HILBY + braState, opCol * HILBY + ketState) * FB;
 								end
+								BWFB = BWFB + conjB(conjRow, conjCol, braState) * WFB;
 							end
-							BWFB = BWFB + conjB(conjRow, conjCol, braState) * WFB;
 						end
+						rightBlock(conjCol, row, opRow + 1) = rightBlock(conjCol, row, opRow + 1) + BWFB;
 					end
-					rightBlock(conjCol, row, opRow + 1) = rightBlock(conjCol, row, opRow + 1) + BWFB;
 				end
 			end
-		end
-		inner = rightBlock;
-		%fprintf('site %d contracted\n', site);
-	end	%site
+			inner = rightBlock;
+			%fprintf('site %d contracted\n', site);
+		end	%site
 	end	% if in case TARGET == L
 end	%function
