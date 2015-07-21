6aa9f5618f56a46de306548f17cacacefd06fbed
Oliver Thomson Brown <otbrown91@gmail.com>
2015-07-21 11:43:44 +0100

    CompMPSTest.m: overhaul -- converted to class-based

--- testing/CompMPSTest.m
+++ testing/CompMPSTest.m
@@ -3,96 +3,63 @@
 % Oliver Thomson Brown
 % 2015-07-17
 
-% test matrix product states
-mps270 = CompMPS(2, 7, 0);      % uncompressed
-mps360 = CompMPS(3, 6, 0);
-mps450 = CompMPS(4, 5, 0);
-mps550 = CompMPS(5, 5, 0);
-mps272 = CompMPS(2, 7, 2);      % compressed
-mps366 = CompMPS(3, 6, 6);
-mps4512 = CompMPS(4, 5, 12);
-mps5515 = CompMPS(5, 5, 15);
+classdef CompMPSTest < matlab.unittest.TestCase
 
-% absolute tolerance 
-% no actual floating point testing here because we're just checking size, shape, and types, so tol = 0
-tol = 0;
+    properties
+        testMPS;
+        testMPSLength;
+        testCOMPRESS;
+        testHILBY;
+    end
 
-%% type-check
-assert(isa(mps270,'cell'), 'Fundamental problem: CompMPS is not returning cell arrays.');
-assert(isa(mps360,'cell'), 'Fundamental problem: CompMPS is not returning cell arrays.');
-assert(isa(mps450,'cell'), 'Fundamental problem: CompMPS is not returning cell arrays.');
-assert(isa(mps550,'cell'), 'Fundamental problem: CompMPS is not returning cell arrays.');
-assert(isa(mps272,'cell'), 'Fundamental problem: CompMPS is not returning cell arrays.');
-assert(isa(mps366,'cell'), 'Fundamental problem: CompMPS is not returning cell arrays.');
-assert(isa(mps4512,'cell'), 'Fundamental problem: CompMPS is not returning cell arrays.');
-assert(isa(mps5515,'cell'), 'Fundamental problem: CompMPS is not returning cell arrays.');
+    properties (MethodSetupParameter)
+        HILBY = {2, 2, 3, 4, 5, 2, 3, 4, 5};
+        mpsLength = {5, 7, 6, 5, 5, 7, 6, 5, 5};
+        COMPRESS = {0, 0, 0, 0, 0, 2, 6, 12, 15};
+    end
 
-%% cell-shape-check
-sz270 = size(mps270);
-assert(sz270(1) == 7, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-assert(sz270(2) == 1, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-sz360 = size(mps360);
-assert(sz360(1) == 6, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-assert(sz360(2) == 1, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-sz450 = size(mps450);
-assert(sz450(1) == 5, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-assert(sz450(2) == 1, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-sz550 = size(mps550);
-assert(sz550(1) == 5, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-assert(sz550(2) == 1, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-sz272 = size(mps272);
-assert(sz272(1) == 7, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-assert(sz272(2) == 1, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-sz366 = size(mps366);
-assert(sz366(1) == 6, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-assert(sz366(2) == 1, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-sz4512 = size(mps4512);
-assert(sz4512(1) == 5, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-assert(sz4512(2) == 1, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-sz5515 = size(mps5515);
-assert(sz5515(1) == 5, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
-assert(sz5515(2) == 1, 'Fundamental problem: CompMPS is returning cell arrays of the wrong shape.');
+    methods (TestMethodSetup, ParameterCombination='sequential')
+        function MethodSetup(testCase, HILBY, mpsLength, COMPRESS)
+            testCase.testMPS = CompMPS(HILBY, mpsLength, COMPRESS);
+            testCase.testMPSLength = mpsLength;
+            testCase.testCOMPRESS = COMPRESS;
+            testCase.testHILBY = HILBY;
+        end
+    end
 
-%% complex-check
-for site = 1 : 1 : length(mps270)
-    A = mps270{site};
-    assert(~isreal(A), 'Error: CompMPS is returning real-valued matrices');
-end
-for site = 1 : 1 : length(mps360)
-    A = mps360{site};
-    assert(~isreal(A), 'Error: CompMPS is returning real-valued matrices');
-end
-for site = 1 : 1 : length(mps450)
-    A = mps450{site};
-    assert(~isreal(A), 'Error: CompMPS is returning real-valued matrices');
-end
-for site = 1 : 1 : length(mps550)
-    A = mps550{site};
-    assert(~isreal(A), 'Error: CompMPS is returning real-valued matrices');
-end
-for site = 1 : 1 : length(mps272)
-    A = mps272{site};
-    assert(~isreal(A), 'Error: CompMPS is returning real-valued matrices');
-end
-for site = 1 : 1 : length(mps366)
-    A = mps366{site};
-    assert(~isreal(A), 'Error: CompMPS is returning real-valued matrices');
-end
-for site = 1 : 1 : length(mps4512)
-    A = mps4512{site};
-    assert(~isreal(A), 'Error: CompMPS is returning real-valued matrices');
-end
-for site = 1 : 1 : length(mps5515)
-    A = mps5515{site};
-    assert(~isreal(A), 'Error: CompMPS is returning real-valued matrices');
-end
+    methods (Test)
+        function testClass(testCase)
+            testCase.assertClass(testCase.testMPS, 'cell');
+        end
 
-%% matrix-shape-check
-testSizes(mps270, 2, 0);
-testSizes(mps360, 3, 0);
-testSizes(mps450, 4, 0);
-testSizes(mps550, 5, 0);
-testSizes(mps272, 2, 2);
-testSizes(mps366, 3, 6);
-testSizes(mps4512, 4, 12);
-testSizes(mps5515, 5, 15);
+        function testCellShape(testCase)
+            testCase.assertSize(testCase.testMPS, [testCase.testMPSLength, 1]);
+        end
+
+        function testComplex(testCase)
+            for site = 1 : 1 : testCase.testMPSLength
+                testCase.assertFalse(isreal(testCase.testMPS{site}));
+            end
+        end
+
+        function testMatShape(testCase)
+            if testCase.testCOMPRESS == 0
+                testCase.testCOMPRESS = Inf;
+            end
+            % initialise rowSz and colSz
+            rowSz = 1;
+            colSz = min(testCase.testHILBY, testCase.testCOMPRESS);
+            % sweep through each site in the matrix product state
+            for site = 1 : 1 : testCase.testMPSLength;
+                testCase.assertSize(testCase.testMPS{site}, [rowSz, colSz, testCase.testHILBY]);
+                % gauge whether there are more sites to the left, or to the right of the current one
+                lLen = site + 1;
+                rLen = testCase.testMPSLength - site - 1;
+                len = min(lLen, rLen);
+                % set next site rowSz and colSz
+                rowSz = colSz;
+                colSz = min(testCase.testCOMPRESS, testCase.testHILBY^len);
+            end
+        end
+    end
+end
