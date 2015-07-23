7d59086396a4360f939be691efa73a239db3df9c
Oliver Thomson Brown <otbrown91@gmail.com>
2015-07-23 13:57:20 +0100

    LCan2.m: test variant of LCan which does a separate svd for each local state

--- dev/LCan2.m
+++ dev/LCan2.m
@@ -14,28 +14,17 @@
 %		  route should be 1 : 1 : L-1 where L is the length of the chain
 
 function [lmps] = LCan(mps, route)
-	% RETURN ALLOCATION
-	lmps = mps;
-	
-    [rowMax, colMax, HILBY] = size(lmps{route(1)});
+    % RETURN ALLOCATION
+    lmps = mps;
     
-	for site = route		
-		perm = permute(lmps{site}, [1, 3, 2]);
-		M = reshape(perm, [HILBY * rowMax, colMax]);
-		[U, S, V] = svd(M);
+    for site = route		
+        for localState = 1 : 1 : HILBY
+            [U, S, V] = svd(lmps{site}(:, :, localState));
+            lmps{site}(:, :, localState) = U;
 
-		U = U(1 : (HILBY * rowMax), 1 : colMax);
-		reshU = reshape(U, [rowMax, HILBY, colMax]);
-		lmps{site} = permute(reshU, [1, 3, 2]);
-
-		chain = S * ctranspose(V);
-
-		rowMax = colMax;
-		colMax = size(lmps{site + 1}(:,:,1), 2);
-		
-		for localState = 1 : 1 : HILBY
-			N = chain * lmps{site+1}(:, :, localState);
-			lmps{site + 1}(:, :, localState) = N(1 : rowMax, 1 : colMax);
-		end 
-	end
+            chain = S * ctranspose(V);
+            lmps{site + 1}(:, :, localState) = ...
+                chain * lmps{site+1}(:, :, localState);
+        end 
+    end
 end
